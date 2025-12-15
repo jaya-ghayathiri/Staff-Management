90873a22ee170f43c7cc303e8a79aaaa3d8fbeb1
jaya-ghayathiri <jaya-ghayathiri.bs2022cse@sece.ac.in>
2025-12-15 14:38:13 +0530

    backend

--- Backend/server.js
+++ Backend/server.js
@@ -11,7 +11,6 @@ app.use(express.json());
 
 app.use("/api/auth", require("./routes/authRoutes"));
 app.use("/api/users", require("./routes/userRoutes"));
-app.use("/api/attendance", require("./routes/attendanceRoutes"));
-app.use("/api/leaves", require("./routes/leaveRoutes"));
+app.use("/api/faculty", require("./routes/facultyRoutes"));
 
 app.listen(5000, () => console.log("Server running on port 5000"));
