3cdcfff4a8dcf213686f65842d21b219bcab6ce9
jaya-ghayathiri <jaya-ghayathiri.bs2022cse@sece.ac.in>
2025-12-16 11:44:03 +0530

    backend

--- Backend/routes/facultyRoutes.js
+++ Backend/routes/facultyRoutes.js
@@ -6,8 +6,8 @@ const { protect } = require("../middleware/authMiddleware");
 router.get("/courses", protect, facultyCtrl.getAssignedCourses);
 router.get("/timetable", protect, facultyCtrl.getTimetable);
 router.post("/attendance", protect, facultyCtrl.markAttendance);
-router.post("/leaves", protect, facultyCtrl.requestLeave);
-router.get("/leaves", protect, facultyCtrl.getLeaveStatus);
+router.post("/post-leaves", protect, facultyCtrl.requestLeave);
+router.get("/get-leaves", protect, facultyCtrl.getLeaveStatus);
 router.get("/students/:courseId", protect, facultyCtrl.getStudentsByCourse);
 
 module.exports = router;
